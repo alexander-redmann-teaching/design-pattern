1cf487c66b384a8eae6cc5365a4d5f660388cfdb
Alexander Redmann <redmann@xplace.de>
2018-06-06 13:24:23 +0200

    add dummy project

---
+++ src/main/java/de/art/examples/designpattern/Main.java
@@ -0,0 +1,16 @@
+package de.art.examples.designpattern;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by redmann on 7/14/15.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
