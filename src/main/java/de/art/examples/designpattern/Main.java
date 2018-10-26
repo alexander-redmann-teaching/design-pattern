05d13aa6cb3c48edf0c7d1a9342f3c7c2b51362f
Alexander Redmann <redmann@xplace.de>
2018-10-26 11:21:34 +0200

    add command example

--- src/main/java/de/art/examples/designpattern/Main.java
+++ src/main/java/de/art/examples/designpattern/Main.java
@@ -4,12 +4,19 @@ package de.art.examples.designpattern;
  * Created by redmann on 7/14/15.
  */
 public class Main {
-
     public static void main(String[] args) {
-        final EmailClient emailClient = new EmailClient();
-        emailClient.emailReceived(new Email("test@gmail.com"));
-        emailClient.emailReceived(new Email("test@businessaddress.com"));
-        emailClient.emailReceived(new Email("test@freemail.net"));
-//        emailClient.emailReceived(new Email("test@other.com"));
+        StockReceiver stockReceiver = new StockReceiver();
+
+        OrderCommandInterface buyStockOrder = new BuyStockCommand(stockReceiver);
+        OrderCommandInterface sellStockOrder = new SellStockCommand(stockReceiver);
+
+        BrokerInvoker broker = new BrokerInvoker();
+        System.out.println("Take orders");
+        broker.takeOrder(buyStockOrder);
+        broker.takeOrder(sellStockOrder);
+        broker.takeOrder(buyStockOrder);
+        broker.takeOrder(buyStockOrder);
+        System.out.println("Place orders");
+        broker.placeOrders();
     }
 }
