444e776884a5756ff0b3b08e5f3be34ba29a5f8b
Alexander Redmann <redmann@xplace.de>
2018-10-26 15:22:18 +0200

    imporove example

--- src/main/java/de/art/examples/designpattern/Main.java
+++ src/main/java/de/art/examples/designpattern/Main.java
@@ -11,6 +11,7 @@ public class Main {
         Expression married = new TerminalExpression("married");
         return new AndExpression(julie, married);
     }
+
     //Rule: Robert and John are male
     public static Expression getMaleExpression() {
         Expression male = new TerminalExpression("male");
@@ -20,12 +21,16 @@ public class Main {
         return new OrExpression(new AndExpression(robert, male), new AndExpression(john, male));
     }
 
+    private static void testSentence(String sentence, Expression expression) {
+        System.out.println(sentence + "? : " + expression.interpret(sentence.split(" ")));
+    }
+
     public static void main(String[] args) {
         Expression isMale = getMaleExpression();
         Expression isMarriedWoman = getMarriedWomanExpression();
 
-        System.out.println("Julie is a married women? " + isMarriedWoman.interpret(new String[]{"Julie", "married"}));
-        System.out.println("John is female? " + isMale.interpret(new String[]{"John", "female"}));
-        System.out.println("Robert is male? " + isMale.interpret(new String[]{"Robert", "male"}));
+        testSentence("Julie is a married women", getMarriedWomanExpression());
+        testSentence("John is female", getMaleExpression());
+        testSentence("Robert is male", getMaleExpression());
     }
 }
