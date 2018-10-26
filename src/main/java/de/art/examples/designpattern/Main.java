a391c4ec216233d16028669144341ad81b24414a
Alexander Redmann <redmann@xplace.de>
2018-10-26 15:13:49 +0200

    add interpreter example

--- src/main/java/de/art/examples/designpattern/Main.java
+++ src/main/java/de/art/examples/designpattern/Main.java
@@ -4,19 +4,28 @@ package de.art.examples.designpattern;
  * Created by redmann on 7/14/15.
  */
 public class Main {
+
+    //Rule: Julie is a married women
+    public static Expression getMarriedWomanExpression() {
+        Expression julie = new TerminalExpression("Julie");
+        Expression married = new TerminalExpression("married");
+        return new AndExpression(julie, married);
+    }
+    //Rule: Robert and John are male
+    public static Expression getMaleExpression() {
+        Expression male = new TerminalExpression("male");
+        Expression robert = new TerminalExpression("Robert");
+        Expression john = new TerminalExpression("John");
+
+        return new OrExpression(new AndExpression(robert, male), new AndExpression(john, male));
+    }
+
     public static void main(String[] args) {
-        StockReceiver stockReceiver = new StockReceiver();
+        Expression isMale = getMaleExpression();
+        Expression isMarriedWoman = getMarriedWomanExpression();
 
-        OrderCommandInterface buyStockOrder = new BuyStockCommand(stockReceiver);
-        OrderCommandInterface sellStockOrder = new SellStockCommand(stockReceiver);
-
-        BrokerInvoker broker = new BrokerInvoker();
-        System.out.println("Take orders");
-        broker.takeOrder(buyStockOrder);
-        broker.takeOrder(sellStockOrder);
-        broker.takeOrder(buyStockOrder);
-        broker.takeOrder(buyStockOrder);
-        System.out.println("Place orders");
-        broker.placeOrders();
+        System.out.println("Julie is a married women? " + isMarriedWoman.interpret(new String[]{"Julie", "married"}));
+        System.out.println("John is female? " + isMale.interpret(new String[]{"John", "female"}));
+        System.out.println("Robert is male? " + isMale.interpret(new String[]{"Robert", "male"}));
     }
 }
