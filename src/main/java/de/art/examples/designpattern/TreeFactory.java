0cf22a87e54eb3b59b91dd8474d9a10a303e4dc9
Alexander Redmann <redmann@xplace.de>
2019-01-29 14:02:00 +0100

    change signature of method

--- src/main/java/de/art/examples/designpattern/TreeFactory.java
+++ src/main/java/de/art/examples/designpattern/TreeFactory.java
@@ -6,10 +6,10 @@ import java.util.Map;
 public class TreeFactory {
     static Map<String, TreeType> treeTypes = new HashMap<String, TreeType>();
 
-    public static TreeType getTreeType(String name, String color, String otherTreeData) {
+    public static TreeType getTreeType(String name, String color, String texture) {
         TreeType result = treeTypes.get(name);
         if (result == null) {
-            result = new TreeType(name, color, otherTreeData);
+            result = new TreeType(name, color, texture);
             treeTypes.put(name, result);
         }
         return result;
--- src/main/java/de/art/examples/designpattern/TreeType.java
+++ src/main/java/de/art/examples/designpattern/TreeType.java
@@ -3,15 +3,15 @@ package de.art.examples.designpattern;
 public class TreeType {
     private String name;
     private String color;
-    private String otherTreeData;
+    private String texture;
 
-    public TreeType(String name, String color, String otherTreeData) {
+    public TreeType(String name, String color, String texture) {
         this.name = name;
         this.color = color;
-        this.otherTreeData = otherTreeData;
+        this.texture = texture;
     }
 
     public void draw(int x, int y) {
-        System.out.println("draw: " + name + " in " + color + " at " + x + "," + y + " with " + otherTreeData);
+        System.out.println("draw: " + name + " in " + color + " at " + x + "," + y + " with " + texture);
     }
 }
