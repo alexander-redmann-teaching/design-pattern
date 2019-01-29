c1279bf267b4d5837db42f89d124ba192d820025
Alexander Redmann <redmann@xplace.de>
2019-01-29 12:28:50 +0100

    change example

--- src/main/java/de/art/examples/designpattern/Main.java
+++ src/main/java/de/art/examples/designpattern/Main.java
@@ -8,6 +8,6 @@ public class Main {
     public static void main(String[] args) {
         DialogFacade dialogFacade = new DialogFacade();
         boolean erase = dialogFacade.showPanel("Erase HDD?");
-
+        System.out.println("Result: " + erase);
     }
 }
