e785a17113b50d85e4800174da3cbd4aebab3004
Alexander Redmann <redmann@xplace.de>
2019-02-08 16:18:55 +0100

    change if statement to switch

--- src/main/java/de/art/examples/designpattern/CeilingFanPullChainClassic.java
+++ src/main/java/de/art/examples/designpattern/CeilingFanPullChainClassic.java
@@ -4,18 +4,23 @@ public class CeilingFanPullChainClassic {
     private int currentState = 0;
 
     public void pull() {
-        if (currentState == 0) {
-            currentState = 1;
-            System.out.println("low speed");
-        } else if (currentState == 1) {
-            currentState = 2;
-            System.out.println("medium speed");
-        } else if (currentState == 2) {
-            currentState = 3;
-            System.out.println("high speed");
-        } else {
-            currentState = 0;
-            System.out.println("turning off");
+        switch (currentState) {
+            case 0:
+                currentState = 1;
+                System.out.println("low speed");
+                break;
+            case 1:
+                currentState = 2;
+                System.out.println("medium speed");
+                break;
+            case 2:
+                currentState = 3;
+                System.out.println("high speed");
+                break;
+            case 3:
+                currentState = 0;
+                System.out.println("turning off");
+                break;
         }
     }
 }
