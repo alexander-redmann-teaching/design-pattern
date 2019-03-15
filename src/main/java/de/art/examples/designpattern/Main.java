dd1bdf8ca910ad6bdfa7fd69110ca1b70a489e2b
Alexander Redmann <redmann@xplace.de>
2019-03-15 13:09:39 +0100

    Remove exception

--- src/main/java/de/art/examples/designpattern/Main.java
+++ src/main/java/de/art/examples/designpattern/Main.java
@@ -5,7 +5,7 @@ package de.art.examples.designpattern;
  */
 public class Main {
 
-    public static void main(String[] args) throws CloneNotSupportedException {
+    public static void main(String[] args) {
         final Animal tiger = Animal.getInstance();
         tiger.setColor("Orange");
         tiger.setLimbs(4);
